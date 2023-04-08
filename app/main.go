2641bad78d473fb7b077e3481cf63bc2f9926181
Shuri-Honda-1101 <shuri.honda065146@gmail.com>
2023-04-08 14:18:49 +0900

    feat:entのセットアップを行なった

--- app/main.go
+++ app/main.go
@@ -1,12 +1,28 @@
 package main
 
 import (
+	"context"
+	"log"
 	"net/http"
 
-	"github.com/labstack/echo/v4"
+	"entgo.io/ent/entc/integration/ent"
+	"github.com/labstack/echo"
 )
 
 func main() {
+	// MySQLデータベースに接続し、Ent ORMライブラリの自動マイグレーションツールを実行して、データベーススキーマを作成
+    client, err := ent.Open("mysql", "<user>:<pass>@tcp(<host>:<port>)/<database>?parseTime=True")
+    if err != nil {
+        log.Fatalf("failed opening connection to mysql: %v", err)
+    }
+	// リソースを解放
+    defer client.Close()
+    // Entの自動マイグレーションツールを実行して、データベーススキーマを作成
+    if err := client.Schema.Create(context.Background()); err != nil {
+        log.Fatalf("failed creating schema resources: %v", err)
+    }
+
+	// Echoのインスタンスを作成
 	e := echo.New()
 	e.GET("/", func(c echo.Context) error {
 		return c.String(http.StatusOK, "Hello, World!")
